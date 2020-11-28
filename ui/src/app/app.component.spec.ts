2965f5e27f38ea635f903d4345a99a2983873860
MLwithSandy <sandeep.ism@gmail.com>
2020-11-28 19:24:52 +0100

    docker packaging done for UI and backend

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ui app is running!');
+    expect(compiled.querySelector('.reco-card-content span').textContent).toContain('ui app is running!');
   });
 });
