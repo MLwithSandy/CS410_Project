d7add4e6f145f34f879a664b937d03c07cd291f7
MLwithSandy <sandeep.ism@gmail.com>
2020-11-28 21:53:26 +0100

    Version change for docker images to 0.0.2

--- StockRatingsSystem/dockerImage.sh
+++ StockRatingsSystem/dockerImage.sh
@@ -2,5 +2,5 @@
 
 version="0.0.2"
 
-docker image build -t skr8050/sre-backend:version .
-docker push skr8050/sre-backend:version
+docker image build -t skr8050/sre-backend:$version .
+docker push skr8050/sre-backend:$version
