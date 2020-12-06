c8da8e6a5b0dba4cbbfe73a10327a0c27e18d44a
MLwithSandy <sandeep.ism@gmail.com>
2020-12-06 20:22:40 +0100

    Final version

--- StockRatingsSystem/dockerImage.sh
+++ StockRatingsSystem/dockerImage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="0.0.2"
+version="0.0.3"
 
 docker image build -t skr8050/sre-backend:$version .
 docker push skr8050/sre-backend:$version
--- ui/dockerImage.sh
+++ ui/dockerImage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="0.0.2"
+version="0.0.3"
 
 echo $version
 docker image build -t skr8050/sre-frontend:$version .
